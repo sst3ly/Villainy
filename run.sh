e160baffd9f53542769f2b18a0afbcae9402ea5e
Tarek <sst3ly@gmail.com>
2026-02-23 15:55:20 -0800

    Added -clean option to run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
+while getopts "clean" opt; do
+    case $opt in
+        clean) rm -rf build ;;
+        \?) echo "Invalid option: -$OPTARG" >&2 ; exit 1;;
+    esac
+done
 ./build.sh
 echo "\n"
 cd build
